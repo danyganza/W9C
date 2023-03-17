a5e81ee5582b27685e11d05465740d74f534f0ef
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-03-16 21:06:33 -0600

    added valiues

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// Create arrays
+let item_names = ['Jordan', 'Nike', 'Puma'];
+let item_prices = [15, 17, 23];
+let in_stock = [true, false, true];
+
+// Creating variables
+let first_item_name = item_names[0];
+let first_item_price = item_prices[0];
+
+// Add new values using push function
+item_names.push('Adidas');
+item_prices.push(29);
+in_stock.push(false);
+
+// Gave last values using pop function
+let last_item_name = item_names.pop();
+let last_item_price = item_prices.pop();
+let last_item_stock = in_stock.pop();
