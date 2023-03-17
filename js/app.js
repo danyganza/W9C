eee61bc2c68ca57f1875dcd95303014543456800
dannyganza <kingdanny@Dannys-MacBook-Air.local>
2023-03-17 16:58:32 -0600

    added values

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-// Create arrays
+// Created arrays to be used
 let item_names = ['Jordan', 'Nike', 'Puma'];
 let item_prices = [15, 17, 23];
 let in_stock = [true, false, true];
 
-// Creating variables
+// Created variables and connecting them to arrays
 let first_item_name = item_names[0];
 let first_item_price = item_prices[0];
 
-// Add new values using push function
+// Added new values using push function
 item_names.push('Adidas');
 item_prices.push(29);
 in_stock.push(false);
